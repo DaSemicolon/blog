f69002c06c138348e0611527fc04b2c30deb7923
Kavindu Wijesuriya <kavinduwiesuriya@gmail.com>
2021-06-25 12:43:27 +0530

    added TM

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/globals.scss';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-T3WFG57',
+};
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  });
+
   return <Component {...pageProps} />;
 }
 
