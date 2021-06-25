2939a3db8bd14317decb8473d751a7332418c9ec
Kavindu Wijesuriya <kavinduwiesuriya@gmail.com>
2021-06-25 16:57:44 +0530

    added proper meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,12 @@ const Home: NextPage<{recentPosts: Array<PrismicNodeBlogPost>}> = ({recentPosts}
       <Head>
         <title>dasemicolon.dev</title>
         <meta name="description" content="Hi! I'm Kavindu, A full stack developer specializing in the front-end. I have 7 years of experience developing scalable web and mobile apps using technologies like JavaScript, TypeScript, NodeJS, GraphQL, React, ReactNative, MySQL, MongoDB, AWS and many more." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dasemicolon.dev/" />
+        <meta property="og:title" content="dasemicolon.dev" />
+        <meta property="og:image" content="https://dasemicolondev.cdn.prismic.io/dasemicolondev/ad33da8f-8849-4368-98c7-25084ea57a8f_logodasemi.svg" />
+        <meta property="og:description" content="Hi! I'm Kavindu, A full stack developer specializing in the front-end. I have 7 years of experience developing scalable web and mobile apps using technologies like JavaScript, TypeScript, NodeJS, GraphQL, React, ReactNative, MySQL, MongoDB, AWS and many more." />
+        <meta property="og:site_name" content="dasemicolon.dev" />
       </Head>
       <div className={style.container}>
         <div className={style.introContainer}>
