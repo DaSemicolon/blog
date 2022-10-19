8d90735896ceac3d16f162fd542619782c68d643
Kavindu <kavinduwijesuriya@gmail.com>
2022-10-19 23:14:04 +0100

    removed tawkto

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,18 @@
 import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
-import tawkTo from "tawkto-react";
 
 import '../styles/globals.scss';
 
 const tagManagerArgs = {
   gtmId: 'GTM-T3WFG57',
 };
-const tawkToPropertyId = "6122189cd6e7610a49b160c8";
-const tawkToKey = "1fdmj09q5";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    tawkTo(tawkToPropertyId, tawkToKey);
     TagManager.initialize(tagManagerArgs);
   }, []);
-  
+
   return <Component {...pageProps} />;
 }
 
