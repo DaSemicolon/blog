1a6e1f7181c92b1c0eeb6cdc6850131a17916b29
Kavindu Wijesuriya <kavinduwijesuriya@gmail.com>
2023-04-25 00:24:37 +0100

    fixed embed issue

--- pages/blog/[uid].tsx
+++ pages/blog/[uid].tsx
@@ -7,44 +7,79 @@ import { getAllBlogPostIds, getPostByUID } from "../../utils/blog-post";
 import style from "./index.module.scss";
 import React from "react";
 
-const Post: NextPage<{blogPost: PrismicNodeBlogPost}> = ({blogPost}) => {
+const Post: NextPage<{ blogPost: PrismicNodeBlogPost }> = ({ blogPost }) => {
   const title = RichText.asText(blogPost.title) || "Untitled";
   return (
     <MainLayout>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={RichText.asText(blogPost.description)} />
+        <meta
+          name="description"
+          content={RichText.asText(blogPost.description)}
+        />
         <meta property="og:title" content={title} />
         <meta property="og:image" content={blogPost.cover_image.url} />
-        <meta property="og:description" content={RichText.asText(blogPost.description)} />
-        <script async defer src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?skin=desert"></script>
+        <meta
+          property="og:description"
+          content={RichText.asText(blogPost.description)}
+        />
+        <script
+          async
+          defer
+          src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?skin=desert"
+        ></script>
       </Head>
       <h1 className={style.title}>{title}</h1>
-      <div className={style.author}>{RichText.asText(blogPost.authors.name)} | {new Date(blogPost._meta.firstPublicationDate).toDateString()}</div>
+      <div className={style.author}>
+        {RichText.asText(blogPost.authors.name)} |{" "}
+        {new Date(blogPost._meta.firstPublicationDate).toDateString()}
+      </div>
       {
         <div className={style.tagContainer}>
-          {
-            blogPost._meta.tags.map((tag) => (
-              <Tag key={tag} text={tag} />
-            ))
-          }
+          {blogPost._meta.tags.map((tag) => (
+            <Tag key={tag} text={tag} />
+          ))}
         </div>
       }
       <div className={style.content}>
-        <RichText render={blogPost.content} htmlSerializer={(type, element, content, children, key) => {
-          let props = {};
-          switch(type) {
-          case Elements.preformatted:
-            props = {className: "prettyprint"};
-            return React.createElement('pre', {...props, key}, React.createElement('code', {}, children));
-          default:
-            return null;
-          }
-        }} />
+        <RichText
+          render={blogPost.content}
+          htmlSerializer={(type, element, content, children, key) => {
+            let props = {};
+            switch (type) {
+            case Elements.preformatted:
+              props = { className: "prettyprint" };
+              return React.createElement(
+                "pre",
+                { ...props, key },
+                React.createElement("code", {}, children)
+              );
+            case Elements.embed:
+              console.log(element);
+              return (
+                <iframe
+                  key={key}
+                  className={style.embd} 
+                  src={`https://www.youtube.com/embed/${element.oembed.embed_url.split("v=").reverse()[0]}?feature=oembed`}
+                  title="YouTube video player" 
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+              );
+            default:
+              return null;
+            }
+          }}
+        />
       </div>
-      {blogPost.codepen && <div className={style.codepen} dangerouslySetInnerHTML={{__html: blogPost.codepen.html}} />}
-    </MainLayout>);
-}; 
+      {blogPost.codepen && (
+        <div
+          className={style.codepen}
+          dangerouslySetInnerHTML={{ __html: blogPost.codepen.html }}
+        />
+      )}
+    </MainLayout>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const uid = context.params.uid as string;
